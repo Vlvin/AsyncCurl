b6059806c9d3cf0dd511948a6f7bcf8203c9ef3d
VLvin <ghostv.work@gmail.com>
2025-04-21 01:13:29 +0400

    Now works as expected

--- main.go
+++ main.go
@@ -14,23 +14,31 @@ type Task struct {
 	Data   string `json:"data"`
 }
 
+func http_request(method string, url string, request_body string) (*http.Response, error) {
+	request_body_reader := strings.NewReader(strings.ToUpper(request_body))
+	r, err := http.NewRequest(method, url, request_body_reader)
+	if err != nil {
+		return nil, err
+	}
+	return http.DefaultClient.Do(r)
+}
+
 func await_task(task Task, w http.ResponseWriter) {
-	request_body := strings.NewReader(strings.ToUpper(task.Data))
-	r, err := http.NewRequest(task.Method, task.Url, request_body)
+	r, err := http_request(task.Method, task.Url, task.Data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	defer r.Body.Close()
 	response_body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	w.Write(response_body)
+	defer r.Body.Close()
 }
 
-func tesk_route(w http.ResponseWriter, r *http.Request) {
+func task_route(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	task := new(Task)
 	err := json.NewDecoder(r.Body).Decode(&task)
@@ -42,16 +50,38 @@ func tesk_route(w http.ResponseWriter, r *http.Request) {
 
 }
 
-const port int = 8080
+type RouteHandler struct {
+	route   string
+	handler func(http.ResponseWriter, *http.Request)
+}
+
+func MkRoute(route string, handler func(http.ResponseWriter, *http.Request)) RouteHandler {
+	return RouteHandler{
+		route,
+		handler,
+	}
+}
+
+func NewSimpleHTTPServer(Address string, handlers ...RouteHandler) http.Server {
+	mux := http.NewServeMux()
+	for _, handler := range handlers {
+		mux.HandleFunc(handler.route, handler.handler)
+	}
+	return http.Server{
+		Addr:    Address,
+		Handler: mux,
+	}
+
+}
+
+const PORT int = 8080
 
 func main() {
 	fmt.Println("Hello Go Servers")
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", tesk_route)
-	server := http.Server{
-		Addr:    fmt.Sprintf(":%d", port),
-		Handler: mux,
-	}
+	server := NewSimpleHTTPServer(
+		fmt.Sprintf(":%d", PORT),
+		MkRoute("/", task_route),
+	)
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Printf("SERVER ERROR: %s", err.Error())
 	}
